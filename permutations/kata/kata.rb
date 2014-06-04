d047781c2f5274bfe77893dd105964a6b8cb6351
Jan-Hettich <janhettich@gmail.com>
2014-06-04 14:06:53 -0700

    removed unused files

--- permutations/kata/kata.rb
+++
@@ -1,8 +0,0 @@
-# Permuatation - rearrangment of {1,..,n}
-#  or a "bijective mapping" of {1,..,n} to itself
-# Cycle - cyclical permutation that may move only
-#  a subset of elements; e.g  1->5, 5->2, 2->1
-
-class P
-
-end
